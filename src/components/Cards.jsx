e8ad4199e5fcb7a7b402475c7399dc01a576c025
noemidasta-03 <noemi.dasta18@gmail.com>
2024-10-23 12:59:30 +0200

    create a event draft card

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,11 +1,51 @@
-import React from "react";
-import "./components/Card.css";
-export function Cards({ title, content, image }) {
+import Card from "./Card";
+import "./Card.css";
+
+export default function Cards() {
   return (
-    <div className="card">
-      {image && <img src={image} alt={title} className="card-image" />}
-      <h2 className="card-title">{title}</h2>
-      <p className="card-content">{content}</p>
+    <div className="cards-container">
+      <Card
+        title="titolo della card"
+        content="questo è un contenutuo della card"
+        image="./img/festival-luci-amsterdam-olanda-hd.jpg"
+      />
+      <Card
+        title="titolo della card"
+        content="questo è un contenutuo della card"
+        image="./img\gettyimages-460767505-612x612.jpg"
+      />
+
+      <Card
+        title="titolo della card"
+        content="questo è un contenutuo della card"
+        image="./img\2024021416353982631.jpg"
+      />
+
+      <Card
+        title="titolo della card"
+        content="questo è un contenutuo della card"
+        image="./img\Dichiarazione-di-Indipendenza-Americana-840x560.webp"
+      />
+      <Card
+        title="titolo della card"
+        content="questo è un contenutuo della card"
+        image="./img/4c1ed5d58db53d404bed6db60f8b81e7.jpg"
+      />
+      <Card
+        title="titolo della card"
+        content="questo è un contenutuo della card"
+        image="./img/carnevale-di-rio.jpg"
+      />
+      <Card
+        title="titolo della card"
+        content="questo è un contenutuo della card"
+        image="./img/Messico-cop-scaled-1.jpg"
+      />
+      <Card
+        title="titolo della card"
+        content="questo è un contenutuo della card"
+        image="./img/Holi-Festival2-600x391.jpg"
+      />
     </div>
   );
 }
