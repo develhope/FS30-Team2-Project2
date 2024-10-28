90e25086cfd7bb87ecb20bd8eee08d3e5f36b79b
acMarchetti <andreacosmamarchetti@yahoo.com>
2024-10-28 15:25:27 +0100

    router

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Cards from "../components/Cards";
+import { MapandCalendar } from "../components/MapandCalendar";
+
+export function Home() {
+  return (
+    <>
+      <MapandCalendar />
+      <Cards />
+    </>
+  );
+}
