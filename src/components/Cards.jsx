472eab19c92a9212e017a3c5dff9154669f48860
acMarchetti <andreacosmamarchetti@yahoo.com>
2024-10-29 12:43:04 +0100

    cardsapi

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,37 +1,33 @@
+import Cardsapi from "../assets/cardsapi";
 import Card from "./Card";
 import "./Card.css";
-import { useEffect, useState } from "react";
-
+import { useCarousel } from "./useCarousel";
 const Cards = () => {
-  const [countryList, setCountryList] = useState([]);
-
-  async function getData() {
-    try {
-      const response = await fetch(
-        "https://api.sampleapis.com/countries/countries"
-      );
-      const data = await response.json();
-      setCountryList(data);
-    } catch (error) {
-      console.error(error);
-    }
-  }
-  useEffect(() => {
-    getData();
-  }, []);
-
+  const { list, goLeft, goRight } = useCarousel(Cardsapi);
   return (
     <div>
       <div className="cards-container">
-        {countryList.map((country) => (
-          <div key={country.id}>
+        <img
+          onClick={goLeft}
+          className="previous"
+          src="src\assets\next.png"
+          alt="previous"
+        />
+        {list.slice(0, 3).map((event) => (
+          <div key={event.id}>
             <Card
-              content={country.name}
-              image={country.media.flag}
-              title={country.capital}
+              content={event.content}
+              image={event.image}
+              title={event.title}
             />
           </div>
         ))}
+        <img
+          onClick={goRight}
+          className="next"
+          src="src\assets\next.png"
+          alt="next"
+        />
       </div>
     </div>
   );
