f17354b5c64bc47df4ab48e9a082a366ed05aa35
noemidasta-03 <noemi.dasta18@gmail.com>
2024-10-29 13:02:48 +0100

    carousel card style

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,12 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Card.css";
 
 export default function Card({ title, content, image }) {
+  const [hidden, setHidden] = useState(true);
+
+  function handleToggle() {
+    {
+      hidden ? setHidden(false) : setHidden(true);
+    }
+  }
   return (
-    <div className="card">
+    <div
+      className="card"
+      style={hidden ? { height: "200px" } : { height: "350px" }}
+      onClick={handleToggle}
+    >
       {image && <img src={image} alt={title} className="card-image" />}
       <h2 className="card-title">{title}</h2>
-      <p className="card-content">{content}</p>
+      {!hidden && <p className="card-content">{content}</p>}
     </div>
   );
 }
