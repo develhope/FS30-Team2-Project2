527885e2cd73143f51304816b64ba8e4185e8734
[Giancosimo] <[giancx91@gmail.com]>
2024-10-31 17:01:50 +0100

    new

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { createContext, useState } from "react";
-import Cards from "./components/Cards";
 import { Footer } from "./components/Footer";
-import { MapandCalendar } from "./components/Map/MapandCalendar";
 import { Navbar } from "./components/Navbar";
 import "./App.css";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
+import { Home } from "./pages/Home";
+import CalendarContent from "./components/CalendarContent";
+import { Events } from "./pages/Events";
+import { Travels } from "./pages/Travels";
 
 export const LanguageContext = createContext("it");
 
@@ -28,12 +31,44 @@ function App() {
           onClick={() => handleSetLang("en")}
         />
       </div>
-      <LanguageContext.Provider value={lang}>
-        <Navbar />
-        <Cards />
-        <MapandCalendar />
-        <Footer />
-      </LanguageContext.Provider>
+      <BrowserRouter>
+        <LanguageContext.Provider value={lang}>
+          <Navbar />
+
+          <Routes>
+            <Route path="/" element={<Home />}></Route>
+            <Route path="/" element={<CalendarContent />}></Route>
+            <Route path="/events" element={<Events />}></Route>
+            <Route path="/travels" element={<Travels />}></Route>
+            <Route
+              path="/reviews"
+              element={
+                <div>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Tenetur quam ea illo! Nam harum, earum aliquid odit minus
+                  aperiam inventore ab deleniti nihil doloremque sed incidunt
+                  natus itaque reprehenderit officiis.
+                </div>
+              }
+            ></Route>
+            <Route
+              path="/*"
+              element={
+                <>
+                  <img
+                    src="https://upload.wikimedia.org/wikipedia/en/5/50/Goofy_Duckipedia.png"
+                    alt="pippo"
+                  />
+                  <div>Goofy has not found the page you were looking for.</div>
+                  <Link to="/">Home?</Link>
+                </>
+              }
+            ></Route>
+          </Routes>
+
+          <Footer />
+        </LanguageContext.Provider>
+      </BrowserRouter>
     </>
   );
 }
--- src/components/Map/MapandCalendar.jsx
+++ src/components/Map/MapandCalendar.jsx
@@ -1,11 +1,11 @@
-import { Calendar } from "../Calendar";
+import CalendarContent from "./CalendarContent";
 import { Map } from "./Map";
 
 export function MapandCalendar() {
   return (
     <>
       <Map />
-      <Calendar />
+      <CalendarContent />
     </>
   );
 }
