b9dca2737144cdb118c691d26899a8f9555c91a7
[Giancosimo] <[giancx91@gmail.com]>
2024-10-31 16:51:22 +0100

    new

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -6,12 +6,12 @@ export function Map() {
       <h1>Mappa</h1>
       <img src="./img/WorldMap.jpg" />
       <div className="continenti">
-        <div className="europa"><a href=""></a></div>
-        <div className="asia"><a href=""></a></div>
-        <div className="africa"><a href=""></a></div>
-        <div className="oceania"><a href=""></a></div>
-        <div className="america-nord"><a href=""></a></div>
-        <div className="america-sud"><a href=""></a></div>
+        <div className="europa"><a href=""></a>europa</div>
+        <div className="asia"><a href=""></a>asia</div>
+        <div className="africa"><a href=""></a>africa</div>
+        <div className="oceania"><a href=""></a>oceania</div>
+        <div className="america-nord"><a href=""></a>america del nord</div>
+        <div className="america-sud"><a href=""></a>america del sud</div>
       </div>
     </div>
 
