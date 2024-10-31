ac210d70d5d9185bf3987d948dd8eeccc8dd5857
acMarchetti <andreacosmamarchetti@yahoo.com>
2024-10-31 17:27:38 +0100

    bordermap

--- src/components/MapandCalendar.jsx
+++ src/components/MapandCalendar.jsx
@@ -1,12 +1,17 @@
 import CalendarContent from "./CalendarContent";
 import { Map } from "./Map/Map";
 
-
 export function MapandCalendar() {
   return (
     <>
-      <Map />
-      <CalendarContent />
+      <div
+        style={{
+          display: "flex",
+        }}
+      >
+        <CalendarContent />
+        <Map />
+      </div>
     </>
   );
 }
