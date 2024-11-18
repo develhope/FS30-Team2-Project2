53821091f217a797c0fec7c4c07009cf3649e661
[Giancosimo] <[giancx91@gmail.com]>
2024-11-18 18:04:37 +0100

    Map Reloaded

---
+++ src/components/Map/MapAndCalendar/MapandCalendar.jsx
@@ -0,0 +1,14 @@
+import CalendarContent from "../../CalendarContent";
+import { Map } from "../Map";
+import "./MapandCalendar.scss"
+
+export function MapandCalendar() {
+  return (
+    <>
+      <div>
+        <CalendarContent />
+        <Map />
+      </div >
+    </>
+  );
+}
