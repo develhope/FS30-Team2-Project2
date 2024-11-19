5825f7e3a3b3a8631958ddaf2befbc731f4f0646
noemidasta-03 <noemi.dasta18@gmail.com>
2024-11-19 22:23:58 +0100

    modified cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -19,7 +19,11 @@ export default function Card({ title, content, image, data }) {
         {image && <img src={image} alt={title} className="card-image" />}
         <h2 className="card-title">{title}</h2>
         {!hidden && <p className="card-content">{content}</p>}
-        <span>{new Date(data).toLocaleDateString()}</span>
+        {!hidden && (
+          <span className="cards-date">
+            {new Date(data).toLocaleDateString()}
+          </span>
+        )}
       </div>
     </div>
   );
