48a6fdab4fa2d07bcb22725d5e1767ee7f253b14
acMarchetti <andreacosmamarchetti@yahoo.com>
2024-12-04 14:14:07 +0100

    fix namerica

--- src/pages/continenti/Namerica.jsx
+++ src/pages/continenti/Namerica.jsx
@@ -7,18 +7,14 @@ export function Namerica() {
   return (
     <>
       <Animation textEng={"North America"} textIt={"Nord America"} />
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique qui a
-        animi eius rerum odit quo iure nam sint tempore ullam suscipit porro
-        neque quidem, officiis tenetur nulla libero repellat?
-      </p>
-      {/* <div>
+
+      <div>
         {eventi
           .filter((evento) => evento.continente === "Nord America")
-          .map((evento, index) => {
-            <EventsArray evento={evento} key={index} />;
-          })}
-      </div> */}
+          .map((evento, index) => (
+            <EventsArray evento={evento} key={index} />
+          ))}
+      </div>
     </>
   );
 }
