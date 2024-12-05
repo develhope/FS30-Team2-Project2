55e31d72d46668b7fcf494584716fb2eb76d47a1
noemidasta-03 <noemi.dasta18@gmail.com>
2024-12-05 13:00:52 +0100

    creazione pop up payment

--- src/components/PopUpPayment.jsx
+++ src/components/PopUpPayment.jsx
@@ -1,7 +1,36 @@
 import React from "react";
 import "./PopUpPayment.css";
+import { useState } from "react";
 
 export default function PopUpPayment({ onClick }) {
+  const [formData, setFormData] = useState({
+    email: "",
+    name: "",
+    surname: "",
+    phoneNumber: "",
+    password: "",
+    confirmPassword: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (formData.password !== formData.confirmPassword) {
+      alert("Le password non corrispondono!");
+    } else {
+      alert("Registrazione completata con successo!");
+
+      onClick();
+    }
+  };
+
   function handlePaymentClosed() {
     onClick();
   }
@@ -12,6 +41,86 @@ export default function PopUpPayment({ onClick }) {
         <button onClick={handlePaymentClosed} className="button-pop-up-payment">
           X
         </button>
+        <h2>Registrazione</h2>
+        <form onSubmit={handleSubmit} className="registration-form">
+          <div className="form-group">
+            <label htmlFor="email">Email</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <h3>Indirizzo di fatturazione</h3>
+
+          <div className="form-group">
+            <label htmlFor="name">Nome</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="surname">Cognome</label>
+            <input
+              type="text"
+              id="surname"
+              name="surname"
+              value={formData.surname}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="phoneNumber">Numero di Telefono</label>
+            <input
+              type="text"
+              id="phoneNumber"
+              name="phoneNumber"
+              value={formData.phoneNumber}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="confirmPassword">Conferma Password</label>
+            <input
+              type="password"
+              id="confirmPassword"
+              name="confirmPassword"
+              value={formData.confirmPassword}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <button type="submit" className="submit-button">
+            Registrati
+          </button>
+        </form>
       </div>
     </div>
   );
